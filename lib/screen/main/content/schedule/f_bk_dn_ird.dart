cd5b1c63a2c3ac0a067f00fabab044b3f2766aeb
Eui9179 <outdark97@gmail.com>
2025-03-26 13:22:46 +0900

    Feature/bk-dn-ird

---
+++ lib/screen/main/content/schedule/f_bk_dn_ird.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sophia_scheduler/data/date_info.dart';
+
+class BkDnIrdFrame extends StatelessWidget {
+  const BkDnIrdFrame({super.key, required this.dateInfos});
+
+  final List<DateInfo> dateInfos;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
